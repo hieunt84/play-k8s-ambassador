737062ff233e508796f5e44c4426c4cc5cfd14c2
HieuNT <hieunt9@gmail.com>
2021-10-11 10:26:51 +0700

    update 11Oct2021

--- 05.myapp1/install.sh
+++ 05.myapp1/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # deploy
 kubectl create ns myapp1
-kubectl config set-context --current --namespace myapp2
-kubectl apply -f ./1.deployment.yaml 
-kubectl apply -f ./2.service.yaml
-kubectl apply -f ./3.1.mapping.yaml
+kubectl apply -f ./1.deployment.yaml --namespace myapp1
+kubectl apply -f ./2.service.yaml --namespace myapp1
+kubectl apply -f ./3.1.mapping.yaml --namespace myapp1
 
